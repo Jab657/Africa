4d9d91c3079a17fe8dd231b1afb03182a4982e06
Jacob Colombo <jacob.colombo657@gmail.com>
2023-05-01 10:40:13 -0500

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Jacob Colombo on 5/1/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
